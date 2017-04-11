fc8d359b30f98acef996ee753fb81330d3298658
Ryan Michela <rmichela@salesforce.com>
2017-04-10 18:38:43 -0700

    Updating copyright to BSD 3-Clause

--- grpc-contrib/src/main/java/com/salesforce/grpc/contrib/FutureChain.java
+++ grpc-contrib/src/main/java/com/salesforce/grpc/contrib/FutureChain.java
@@ -1,7 +1,8 @@
 /*
- * Copyright, 1999-2017, SALESFORCE.com
- * All Rights Reserved
- * Company Confidential
+ *  Copyright (c) 2017, salesforce.com, inc.
+ *  All rights reserved.
+ *  Licensed under the BSD 3-Clause license.
+ *  For full license text, see LICENSE.txt file in the repo root  or https://opensource.org/licenses/BSD-3-Clause
  */
 
 package com.salesforce.grpc.contrib;
--- grpc-spring/src/test/java/com/salesforce/grpc/contrib/spring/GrpcServerHostTest.java
+++ grpc-spring/src/test/java/com/salesforce/grpc/contrib/spring/GrpcServerHostTest.java
@@ -1,3 +1,10 @@
+/*
+ *  Copyright (c) 2017, salesforce.com, inc.
+ *  All rights reserved.
+ *  Licensed under the BSD 3-Clause license.
+ *  For full license text, see LICENSE.txt file in the repo root  or https://opensource.org/licenses/BSD-3-Clause
+ */
+
 package com.salesforce.grpc.contrib.spring;/*
  * Copyright, 1999-2017, SALESFORCE.com
  * All Rights Reserved

ad7d2a02b18fd82f06be7d0e91b419ea02fb758b
Ryan Michela <rmichela@salesforce.com>
2017-04-26 17:12:47 -0700

    Implemented FallbackResolver

---
+++ grpc-contrib/src/main/java/com/salesforce/grpc/contrib/FallbackResolver.java
@@ -0,0 +1,75 @@
+/*
+ *  Copyright (c) 2017, salesforce.com, inc.
+ *  All rights reserved.
+ *  Licensed under the BSD 3-Clause license.
+ *  For full license text, see LICENSE.txt file in the repo root  or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+package com.salesforce.grpc.contrib;
+
+import com.google.common.base.Preconditions;
+import io.grpc.*;
+import io.grpc.internal.DnsNameResolverProvider;
+
+import javax.annotation.Nonnull;
+import java.net.URI;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * FallbackResolver is a {@link NameResolver.Factory} that allows multiple {@link NameResolver} instances to be
+ * queried in sequence when attempting to resolve an Authority by URI scheme. If the first name resolver cannot handle
+ * the Authority's scheme, the remaining resolvers will be tried in turn.
+ * <p>
+ * FallbackResolver is particularly useful when setting an explicit client resolver via
+ * {@link ManagedChannelBuilder#nameResolverFactory(NameResolver.Factory)}, rather than relying on gRPC's default
+ * resolver discovery system.
+ * <p>
+ * FallbackResolver assumes the default scheme of the first resolver in the sequence.
+ */
+public class FallbackResolver extends NameResolver.Factory {
+    /**
+     * A shortcut for accessing the default {@link DnsNameResolverProvider}.
+     */
+    public static final NameResolverProvider DNS = new DnsNameResolverProvider();
+
+    private final List<NameResolverProvider> providers = new LinkedList<>();
+
+    /**
+     * Establishes the first {@link NameResolverProvider} to check.
+     */
+    public static FallbackResolver startWith(NameResolverProvider first) {
+        Preconditions.checkNotNull(first, "first");
+        return new FallbackResolver(first);
+    }
+
+    private FallbackResolver(@Nonnull NameResolverProvider first) {
+        thenCheck(first);
+    }
+
+    /**
+     * Establishes a subsequent {@link NameResolverProvider} to check.
+     */
+    public FallbackResolver thenCheck(@Nonnull NameResolverProvider provider) {
+        Preconditions.checkNotNull(provider, "provider");
+        providers.add(provider);
+
+        return this;
+    }
+
+    @Override
+    public NameResolver newNameResolver(URI targetUri, Attributes params) {
+        for (NameResolverProvider provider : providers) {
+            NameResolver resolver = provider.newNameResolver(targetUri, params);
+            if (resolver != null) {
+                return resolver;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String getDefaultScheme() {
+        return providers.get(0).getDefaultScheme();
+    }
+}
---
+++ grpc-contrib/src/test/java/com/salesforce/grpc/contrib/FallbackResolverTest.java
@@ -0,0 +1,111 @@
+/*
+ *  Copyright (c) 2017, salesforce.com, inc.
+ *  All rights reserved.
+ *  Licensed under the BSD 3-Clause license.
+ *  For full license text, see LICENSE.txt file in the repo root  or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+package com.salesforce.grpc.contrib;
+
+import io.grpc.Attributes;
+import io.grpc.NameResolver;
+import io.grpc.NameResolverProvider;
+import org.junit.Test;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.net.URI;
+
+import static org.junit.Assert.*;
+
+@SuppressWarnings("ALL")
+public class FallbackResolverTest {
+    @Test
+    public void firstShouldFind() throws Exception {
+        NameResolver fakeResolver = new FakeResolver();
+        FakeResolverProvider canResolve = new FakeResolverProvider("aaa://", fakeResolver);
+        FakeResolverProvider cannotResolve = new FakeResolverProvider("bbb://", null);
+
+        NameResolver.Factory factory = FallbackResolver.startWith(canResolve).thenCheck(cannotResolve);
+
+        assertEquals(fakeResolver, factory.newNameResolver(new URI("aaa://foo"), Attributes.EMPTY));
+    }
+
+    @Test
+    public void secondShouldFind() throws Exception {
+        NameResolver fakeResolver = new FakeResolver();
+        FakeResolverProvider canResolve = new FakeResolverProvider("aaa://", fakeResolver);
+        FakeResolverProvider cannotResolve = new FakeResolverProvider("bbb://", null);
+
+        NameResolver.Factory factory = FallbackResolver.startWith(cannotResolve).thenCheck(canResolve);
+
+        assertEquals(fakeResolver, factory.newNameResolver(new URI("bbb://foo"), Attributes.EMPTY));
+    }
+
+    @Test
+    public void neitherShouldFind() throws Exception {
+        FakeResolverProvider cannotResolve = new FakeResolverProvider("bbb://", null);
+
+        NameResolver.Factory factory = FallbackResolver.startWith(cannotResolve).thenCheck(cannotResolve);
+
+        assertNull(factory.newNameResolver(new URI("bbb://foo"), Attributes.EMPTY));
+    }
+
+    @Test
+    public void firstSchemeIsDefaultScheme() {
+        NameResolver fakeResolver = new FakeResolver();
+        FakeResolverProvider canResolve = new FakeResolverProvider("aaa://", fakeResolver);
+        FakeResolverProvider cannotResolve = new FakeResolverProvider("bbb://", null);
+
+        NameResolver.Factory factory = FallbackResolver.startWith(canResolve).thenCheck(cannotResolve);
+
+        assertEquals("aaa://", factory.getDefaultScheme());
+    }
+
+    private static class FakeResolverProvider extends NameResolverProvider {
+        private final String scheme;
+        private final NameResolver resolver;
+
+        public FakeResolverProvider(String scheme, NameResolver resolver) {
+            this.scheme = scheme;
+            this.resolver = resolver;
+        }
+
+        @Override
+        protected boolean isAvailable() {
+            return true;
+        }
+
+        @Override
+        protected int priority() {
+            return 0;
+        }
+
+        @Override
+        public NameResolver newNameResolver(URI targetUri, Attributes params) {
+            return resolver;
+        }
+
+        @Override
+        public String getDefaultScheme() {
+            return scheme;
+        }
+    }
+
+    private static class FakeResolver extends NameResolver {
+
+        @Override
+        public String getServiceAuthority() {
+            throw new NotImplementedException();
+        }
+
+        @Override
+        public void start(Listener listener) {
+            throw new NotImplementedException();
+        }
+
+        @Override
+        public void shutdown() {
+            throw new NotImplementedException();
+        }
+    }
+}

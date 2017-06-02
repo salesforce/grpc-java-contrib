5752e007af0665671875a46caebce8c77b63e16e
Ryan Michela <rmichela@salesforce.com>
2017-06-02 09:17:27 -0700

    Begin backpressure testing

---
+++ rxgrpc/rxgrpc-test/src/test/java/com/salesforce/rxgrpc/BackpressureTest.java
@@ -0,0 +1,119 @@
+/*
+ *  Copyright (c) 2017, salesforce.com, inc.
+ *  All rights reserved.
+ *  Licensed under the BSD 3-Clause license.
+ *  For full license text, see LICENSE.txt file in the repo root  or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+package com.salesforce.rxgrpc;
+
+import com.google.protobuf.Empty;
+import com.salesforce.servicelibs.NumberProto;
+import com.salesforce.servicelibs.RxNumbersGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.reactivex.Flowable;
+import io.reactivex.Single;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class BackpressureTest {
+    private static Server server;
+    private static ManagedChannel channel;
+
+    @BeforeClass
+    public static void setupServer() throws Exception {
+        RxNumbersGrpc.NumbersImplBase svc = new RxNumbersGrpc.NumbersImplBase() {
+            @Override
+            public Single<NumberProto.Number> requestPressure(Flowable<NumberProto.Number> request) {
+                return request
+                        .map(proto -> proto.getNumber(0))
+                        .doOnNext(i -> {
+                            System.out.println("    --> " + i);
+                            try { Thread.sleep(150); } catch (InterruptedException e) {}
+                        })
+                        .last(-1)
+                        .map(BackpressureTest::protoNum);
+            }
+
+            @Override
+            public Flowable<NumberProto.Number> responsePressure(Single<Empty> request) {
+                return Flowable
+                        .fromIterable(new Sequence(500))
+                        .doOnNext(i -> System.out.println("   <-- " + i))
+                        .map(BackpressureTest::protoNum);
+            }
+
+            @Override
+            public Flowable<NumberProto.Number> twoWayPressure(Flowable<NumberProto.Number> request) {
+                request.subscribe(
+                        n -> System.out.println("   --> " + n),
+                        Throwable::printStackTrace,
+                        () -> System.out.println("Server done.")
+                );
+
+                return Flowable
+                        .fromIterable(new Sequence(500))
+                        .doOnNext(i -> System.out.println("   <-- " + i))
+                        .map(BackpressureTest::protoNum);
+            }
+        };
+
+        server = ServerBuilder.forPort(0).addService(svc).build().start();
+        channel = ManagedChannelBuilder.forAddress("localhost", server.getPort()).usePlaintext(true).build();
+
+    }
+
+    @AfterClass
+    public static void stopServer() {
+        server.shutdown();
+        channel.shutdown();
+    }
+
+    @Test
+    public void ClientToServerBackpressure() throws InterruptedException {
+        Object lock = new Object();
+
+        RxNumbersGrpc.RxNumbersStub stub = RxNumbersGrpc.newRxStub(channel);
+
+        Flowable<NumberProto.Number> rxRequest = Flowable
+                .fromIterable(new Sequence(500))
+                .doOnNext(i -> System.out.println(i + " -->"))
+                .map(BackpressureTest::protoNum);
+
+        Single<NumberProto.Number> rxResponse = stub.requestPressure(rxRequest);
+
+        rxResponse.subscribe(
+                n -> {
+                    System.out.println("Client done. " + n.getNumber(0));
+                    synchronized (lock) {
+                        lock.notify();
+                    }
+                },
+                t -> {
+                    t.printStackTrace();
+                    synchronized (lock) {
+                        lock.notify();
+                    }
+                });
+
+        synchronized (lock) {
+            lock.wait();
+        }
+    }
+
+    private static NumberProto.Number protoNum(int i) {
+        Random r = ThreadLocalRandom.current();
+        Integer[] ints = new Integer[r.nextInt(64) * 1024 + 1];
+        Arrays.setAll(ints, operand -> i);
+
+        return NumberProto.Number.newBuilder().addAllNumber(Arrays.asList(ints)).build();
+    }
+}
---
+++ rxgrpc/rxgrpc-test/src/test/java/com/salesforce/rxgrpc/Sequence.java
@@ -0,0 +1,37 @@
+/*
+ *  Copyright (c) 2017, salesforce.com, inc.
+ *  All rights reserved.
+ *  Licensed under the BSD 3-Clause license.
+ *  For full license text, see LICENSE.txt file in the repo root  or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+package com.salesforce.rxgrpc;
+
+import java.util.Iterator;
+
+public class Sequence implements Iterable<Integer> {
+    private final int max;
+
+    public Sequence(int max) {
+        this.max = max;
+    }
+
+    public Iterator<Integer> iterator() {
+        return new Iterator<Integer>() {
+            int i = 0;
+
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+
+            public boolean hasNext() {
+                return i < max;
+            }
+
+            public Integer next() {
+                try { Thread.sleep(50); } catch (InterruptedException e) {}
+                return i++;
+            }
+        };
+    }
+}
